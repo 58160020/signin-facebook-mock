2c394edc7119731d8d9e8ef35d7a4c0bb72eaeb2
Nattanich toopsamoot <58160020@go.buu.ac.th>
2017-09-23 12:04:19 +0700

    test pass1

--- facebook.spec.js
+++ facebook.spec.js
@@ -1,39 +1,33 @@
 function Auth(authService) {
     this.authService = authService
 
-    this.signInWithFacebook = (phone,password) => {
-         const returnObjjest ={
-        name: 'Nattanich',
-        facebookId: 963852741,
-        email: 'Nattanich27@gmail.com'
-         }
-
-         return returnObjjest
+    this.signInWithFacebook = (phone, password) => {
+        return this.authService(phone, password)
     }
 }
 
-
-
-test('Mock wtih 1 should return 1', () =>{
+test('Mock wtih 1 should return 1', () => {
     const mockFn = jest.fn(1)
-    .mockReturnValue(1)
+        .mockReturnValue(1)
 
     expect(mockFn(1)).toBe(1)
     expect(mockFn).toBeCalledWith(1)
 })
 
-test('Signin with valid credential shold pass', () => {
-    const returnObjjest ={
-        name: 'Nattanich',
-        facebookId: 963852741,
-        email: 'Nattanich27@gmail.com'
+test('Signin with valid credential should pass', () => {
+    const returnObject = {
+        name: 'Weera',
+        facebookId: '963852741',
+        email: 'wkasetsin@gmail.com'
     }
 
-    const mockFacebook = jest.fn('081777777','abc12345')
-    .mockReturnValue(returnObjjest)
+    const mockFacbook = jest.fn('0817777777', 'abc12345')
+        .mockReturnValue(returnObject)
 
-    let app = new Auth(mockFacebook)
-    let value = app.signInWithFacebook('081777777','abc12345')
+    let app = new Auth(mockFacbook)
+    let value = app.signInWithFacebook('0817777777', 'abc12345')
 
-    expect(value).toEqual(returnObjjest)
+    expect(value).toEqual(returnObject)
+    expect(mockFacbook).toBeCalled()
+    expect(mockFacbook).toBeCalledWith('0817777777', 'abc12345')
 })
